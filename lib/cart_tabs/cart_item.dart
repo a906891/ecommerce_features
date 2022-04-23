3a0fb4c9bb0b88406280503b905c0a9ab56b7912
a906891 <a906891@gmail.com>
2022-04-23 16:45:50 +0530

    Started

--- lib/cart_tabs/cart_item.dart
+++ lib/cart_tabs/cart_item.dart
@@ -8,6 +8,12 @@ class CartItem extends StatefulWidget {
 }
 
 class _CartItemState extends State<CartItem> {
+  String? nameOfItem = "Monthly Pass";
+  int? amountOfItem = 29;
+  String? preparedBy = "LRNR.in";
+  String? imageOfItem = "images/monthlypass.png";
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -18,7 +24,7 @@ class _CartItemState extends State<CartItem> {
             children:  [
 
               ClipRRect(
-                child: Image.asset("images/monthlypass.png",
+                child: Image.asset(imageOfItem!,
                   height: 80.0,),
                 borderRadius: const BorderRadius.all(Radius.circular(10)),
               ),
@@ -28,23 +34,23 @@ class _CartItemState extends State<CartItem> {
               Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text("Monthly Pass",
-                    style: TextStyle(
+                children: [
+                  Text(nameOfItem!,
+                    style: const TextStyle(
                         fontWeight: FontWeight.bold
                     ),),
 
                   SizedBox(height: 5.0,),
 
-                  Text("Rs 29",
-                    style: TextStyle(
+                  Text("Rs $amountOfItem",
+                    style: const TextStyle(
                         fontWeight: FontWeight.bold
                     ),),
 
-                  SizedBox(height: 5.0,),
+                  const SizedBox(height: 5.0,),
 
-                  Text("Prepared by: LRNR.in",
-                    style: TextStyle(
+                  Text("Prepared by: $preparedBy",
+                    style: const TextStyle(
                         color: Colors.grey,
                         fontSize: 12
                     ),),
