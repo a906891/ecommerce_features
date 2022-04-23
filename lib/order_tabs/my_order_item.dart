d97039340634322c985d51bf3c4de8d22296cd06
a906891 <a906891@gmail.com>
2022-04-23 17:22:04 +0530

    Started

--- lib/order_tabs/my_order_item.dart
+++ lib/order_tabs/my_order_item.dart
@@ -8,8 +8,59 @@ class MyOrderItem extends StatefulWidget {
 }
 
 class _MyOrderItemState extends State<MyOrderItem> {
+  String? nameOfItem = "Monthly Pass";
+  int? amountOfItem = 29;
+  String? preparedBy = "LRNR.in";
+  String? imageOfItem = "images/monthlypass.png";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+        children: [
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:  [
+
+              ClipRRect(
+                child: Image.asset(imageOfItem!,
+                  height: 170.0),
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+              ),
+
+              const SizedBox(width: 10.0,),
+
+            ],
+          ),
+
+          const SizedBox(height: 20.0,),
+
+          Row(
+            children: [
+              const SizedBox(width: 40.0,),
+              Text(nameOfItem!,
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  fontSize: 22
+                ),),
+            ],
+          ),
+
+          const SizedBox(height: 40.0,),
+
+          Row(
+            children: [
+              const SizedBox(width: 40.0,),
+              Text("Rs $amountOfItem",
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  color: Colors.black54
+                ),),
+            ],
+          ),
+          const SizedBox(height: 20.0,),
+
+        ]);
   }
 }
--- lib/order_tabs/my_orders.dart
+++ lib/order_tabs/my_orders.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_features/order_tabs/my_order_item.dart';
 import 'package:flutter/material.dart';
 
 class MyOrdersScreen extends StatefulWidget {
@@ -10,6 +11,25 @@ class MyOrdersScreen extends StatefulWidget {
 class _MyOrdersScreenState extends State<MyOrdersScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+
+            children: [
+              ListView.builder(itemBuilder: (buildContext,index){
+
+                return const Card(
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 5),
+                    child: MyOrderItem()
+                );
+              },
+                itemCount: 5,
+                shrinkWrap: true,
+                physics:const NeverScrollableScrollPhysics(),
+              ),
+            ],
+          ),
+        )
+    );
   }
 }
